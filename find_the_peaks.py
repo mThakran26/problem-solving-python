715eedbe96f34b153e0e43cc1cbd38acc25ab7f0
Manisha <mishathakran26@gmail.com>
2024-01-23 14:16:36 +0530

    To find peaks in an array of mountain range

---
+++ find_the_peaks.py
@@ -0,0 +1,10 @@
+# LeetCode Question Link : https://leetcode.com/problems/find-the-peaks/description/
+
+# Solution :
+def findPeaks(self, mountain: list[int]) -> list[int]:
+    peaks = []
+    for i in range(1, len(mountain)-1):
+        if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
+            peaks.append(i)
+
+    return peaks
